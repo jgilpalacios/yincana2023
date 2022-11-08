2ed821a6967341504a911b9239549724b56335c8
jgil19jgil19 <jgil19jgil19@gmail.com>
2022-11-08 11:12:14 +0100

    Genera inscripciones y las guearda

---
+++ public/mijs/encriptador.js
@@ -0,0 +1,193 @@
+/*var clavePub='-----BEGIN PUBLIC KEY-----'+
+'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCd/7HM1m9ei0b5KEUiGb8Eiooq'+
+'UUfB19w8ZKtKzJgpzhb+4qEV58+5TMQZLsQ2XoTF3zRhhkVaItuNRpFVN/5ddNc2'+
+'uxyRKHzzzHTZpFl3ObbQBqqOL4I50my7KAGxF9mgzZjkNQ8raWvq7oYrCpUJaLWt'+
+'dgPdr5akAx0gHf56hwIDAQAB'+
+'-----END PUBLIC KEY-----';*/
+//Ponga aquí la CLAVE PÚBLICA que vaya a utilizar, nio es necesario
+//ya que se carga en index.htm en un texarea oculto.
+var clavePub='-----BEGIN PUBLIC KEY-----'+
+'MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAsQBz4lFIVmrfpcX43FqZ'+
+'Cza7liSZgXe1dxp6PL9eiKrh6Ra3OnrkDD8+snlsRZ/L/BI3311Cl4uSESgI54oR'+
+'9HtQZjlEE298nlJCHy5p0qyv8qWigp5l1gL2djt3Jdmx93Dcmg8ZPE8EwW1RXuUA'+
+'dc8g4qOGHaTJzB/1cDFaWEjpNhgMU7hHBiA1dpg00kq/hTaLYU/Pt53G9RkqANKE'+
+'UvvFty5Mu3XvyYCMqUwOhFS7cixR2LXwQzDaahHQsyCXN/c8jILW86G6Rgu+vasO'+
+'rqM2PobqQtpgNauZ61D0pWin3xTW24tnPAxHIGwT4BAJkYkj7573mzaQQeJcbvjV'+
+'gTczUsEkMpHg6SznKJjrQFfkxPzAmKeKZHh552Ev3n6/UzqwMzMx4t8iwOAA0ilA'+
+'DeQvNP3i8l8tBYEr2LQwdUHlVG6asUwLjLTuamnenAYH6l0ELmhCg6Jil4ceGRW0'+
+'4Lzt5atjiVjXIgbb9UutoQOHZKg5JMoUQHON1jAk6l82iXdrlXJhOC3nXinnhFIP'+
+'f0aEy5gx1uXGbJBWIs9WCg21ZDBfW/gTL54MZJ+yu27rVyOxyuAJ1apBKH0Ro80U'+
+'oWBIUgsrQq4ayNI3om1xn0h57DScqTftbj2f9pPxA1sIHz9cqKIfbbtDNV/IHTiG'+
+'5OOBD5AmNeWSapfy0Ju5wW0CAwEAAQ=='+
+'-----END PUBLIC KEY-----'
+
+/*'-----BEGIN PUBLIC KEY-----'+
+'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDlOJu6TyygqxfWT7eLtGDwajtN'+
+'FOb9I5XRb6khyfD1Yt3YiCgQWMNW649887VGJiGr/L5i2osbl8C9+WJTeucF+S76'+
+'xFxdU6jE0NQ+Z+zEdhUTooNRaY5nZiu5PgDB0ED/ZKBUSLKL7eibMxZtMlUDHjm4'+
+'gwQco1KRMDSmXSMkDwIDAQAB'+
+'-----END PUBLIC KEY-----';*/
+
+/*var clavePriv='-----BEGIN RSA PRIVATE KEY-----'+
+'MIICWwIBAAKBgQCd/7HM1m9ei0b5KEUiGb8EiooqUUfB19w8ZKtKzJgpzhb+4qEV'+
+'58+5TMQZLsQ2XoTF3zRhhkVaItuNRpFVN/5ddNc2uxyRKHzzzHTZpFl3ObbQBqqO'+
+'L4I50my7KAGxF9mgzZjkNQ8raWvq7oYrCpUJaLWtdgPdr5akAx0gHf56hwIDAQAB'+
+'AoGAIXNH/lkL3RVZolh/Qnf29AwsskbhDU4ZJ4Qcq8pjEL68KZxrhEq+tLlm7vny'+
+'upwKOiZGXbtipXGpz+A31rXzGRr3VniZpR3QVaKW+NN/zv2IlRz+IjVcMXOgYc4t'+
+'Sfzrr2AdKt5ddQ/ZD4EGEYZgKqBOxQ5LD10noMwmvjBXcQkCQQDPeSFDl/enZzx6'+
+'lYbuLbax+hYxlUIj05hOir3+/L3mKfm6la1sinECNWgu6zz79/BZSPDvvIdpob/C'+
+'x/hLsbLDAkEAwvQwS4LxvojzBuDm88igkVbeE65W/v/Q/dMqTFTt/LiPCXtrQ9XU'+
+'FKBfeDpxQALe29kzLZ9CfYxHR2hbL5xU7QJAC8hrVTVqQT2ht6yKTziVaaI0ZHh/'+
+'mx1HRx6mD7c26b10i144zTNHm8KAujlt6zl3kvqLhHIGlr8y7tMAjPv4lQJAP6EP'+
+'ejXczQFJwL764ipjLcyaYvYN8uBvx8h6D7vQiSF7ne6oTdWmiKznPAhnGitVrY+w'+
+'gn1uaCe2w/mteVae6QJAJoHC7RMDq1tZy/M9QyvBufsmaXFdpBP7UWXMI+lhRJUR'+
+'SrvlICiufP90uVyYPpJ+rfiFWdbYXynlOzJBD+M6ow=='+
+'-----END RSA PRIVATE KEY-----';*/
+
+//NO SE DEBE SUSTIUIR POR LA CLAVE REAL, ya que esta debe ser custodiada por
+//el propio usuario. solo para la fase de desarrollo y pruebas se puede 
+//incluir la clave privada real
+var clavePriv='-----BEGIN RSA PRIVATE KEY-----'+
+'MIICXQIBAAKBgQDlOJu6TyygqxfWT7eLtGDwajtNFOb9I5XRb6khyfD1Yt3YiCgQ'+
+'WMNW649887VGJiGr/L5i2osbl8C9+WJTeucF+S76xFxdU6jE0NQ+Z+zEdhUTooNR'+
+'aY5nZiu5PgDB0ED/ZKBUSLKL7eibMxZtMlUDHjm4gwQco1KRMDSmXSMkDwIDAQAB'+
+'AoGAfY9LpnuWK5Bs50UVep5c93SJdUi82u7yMx4iHFMc/Z2hfenfYEzu+57fI4fv'+
+'xTQ//5DbzRR/XKb8ulNv6+CHyPF31xk7YOBfkGI8qjLoq06V+FyBfDSwL8KbLyeH'+
+'m7KUZnLNQbk8yGLzB3iYKkRHlmUanQGaNMIJziWOkN+N9dECQQD0ONYRNZeuM8zd'+
+'8XJTSdcIX4a3gy3GGCJxOzv16XHxD03GW6UNLmfPwenKu+cdrQeaqEixrCejXdAF'+
+'z/7+BSMpAkEA8EaSOeP5Xr3ZrbiKzi6TGMwHMvC7HdJxaBJbVRfApFrE0/mPwmP5'+
+'rN7QwjrMY+0+AbXcm8mRQyQ1+IGEembsdwJBAN6az8Rv7QnD/YBvi52POIlRSSIM'+
+'V7SwWvSK4WSMnGb1ZBbhgdg57DXaspcwHsFV7hByQ5BvMtIduHcT14ECfcECQATe'+
+'aTgjFnqE/lQ22Rk0eGaYO80cc643BXVGafNfd9fcvwBMnk0iGX0XRsOozVt5Azil'+
+'psLBYuApa66NcVHJpCECQQDTjI2AQhFc1yRnCU/YgDnSpJVm1nASoRUnU8Jfm3Oz'+
+'uku7JUXcVpt08DFSceCEX9unCuMcT72rAQlLpdZir876'+
+'-----END RSA PRIVATE KEY-----';
+/* Encripta un mensaje de longitud indefinida, lo parte en fragmentos
+   de 117 (1024) o 500 (4096) caracteres encripta los fragmentos y 
+   luego concatena los resultados con el caracter #*/
+
+var longfragmento=450;//ponemos 450 por dejar un margen
+function EncrMensaje(msg){
+	mensaje=msg;
+	var longitud=mensaje.length;
+	var ini=0;
+	var fin=ini+longfragmento;
+	var fragmentos=[];
+	var fi=0;
+	while (fin<longitud){
+		fragmentos[fi]=mensaje.substring(ini, fin);
+		ini=fin;
+		fin=ini+longfragmento;
+		fi++;
+	}
+	fragmentos[fi]=mensaje.substring(ini);
+	var mensajeCRP=''
+	for(var i=0;i<fragmentos.length;i++){
+		if(i>0)mensajeCRP+='#'
+		mensajeCRP+=encripta(fragmentos[i]);
+	
+	}
+	return mensajeCRP;
+}
+/* desencripta un mensaje de longitud indefinida, formado por framentos
+   encriptados de longitud máxima (117) concatenados con el caracter #.
+   regenera los fragmentos dividiendo el mensage por los #, los desencripta
+   y luego los concatena para generar el texto en claro. */
+function DesencrMensaje(msg){
+	var fragCRP=msg.split("#");//alert('llega'+fragCRP.length);
+	var mensajeCLARO='';
+	for (var i=0; i<fragCRP.length;i++){
+		mensajeCLARO+=desencripta(fragCRP[i]);
+	}
+	return mensajeCLARO;
+}
+
+function compacta(){
+var mensaje=tipo+','+nombre_centro+','+loc_centro+','+cod_centro+','+Ap11+','+Ap12+','+Nombre1+','+Edad1;
+mensaje+=','+Ap21+','+Ap22+','+Nombre2+','+Edad2+','+Ap31+','+Ap32+','+Nombre3+','+Edad3+','+Ap41+',';
+mensaje+=Ap42+','+Nombre4+','+Edad4+','+ApP1+','+ApP2+','+NomP+','+TelP1+','+TelP2+','+EmailP;
+var longitud=mensaje.length;
+var ini=0;
+//longfragmento=100;//SE DEFINE COMO VARIABLE GENERALen teoria pueden ser 117 (1024) 500 (4096)
+var fin=ini+longfragmento;
+var fragmentos=[];
+var fi=0;
+while (fin<longitud){
+	fragmentos[fi]=mensaje.substring(ini, fin);
+	ini=fin;
+	fin=ini+longfragmento;
+	fi++;
+}
+fragmentos[fi]=mensaje.substring(ini);
+var mensajeCRP=''
+for(var i=0;i<fragmentos.length;i++){
+	//alert(fragmentos[i]+'('+fragmentos[i].length+')');
+	if(i>0)mensajeCRP+='#'
+	mensajeCRP+=encripta(fragmentos[i]);
+	
+}
+//lo siguiente valdría para pruebas de desencriptado en local 
+//poniendo la clave privada verdadera, no se debe hacer con la 
+//aplicación funcionando en produccion. (se descomenta para pruebas)
+//alert(mensajeCRP);
+/*var fragCRP=mensajeCRP.split("#");
+if(fragCRP==[])fragCRP=[mensajeCRP];
+var mensajeCLARO='';
+for (var i=0; i<fragCRP.length;i++){
+	mensajeCLARO+=desencripta(fragCRP[i]);
+}
+alert(mensajeCLARO);
+//document.getElementById('input').value=mensaje;
+//document.getElementById('output').value=mensajeCRP;*/
+if (document.getElementById('mensajeCRP')){
+	document.getElementById('mensajeCRP').value=mensajeCRP;//cargamos el campo hidden
+	//alert(document.getElementById('mensajeCRP').value);
+}
+//document.getElementById('output2').value=mensajeCLARO;
+//alert('llegamos al final: '+mensajeCRP);
+}
+
+function encripta(texto){//alert(' k')
+	var encrypt = new JSEncrypt();//alert(' k')
+	encrypt.setPublicKey(clavePub);
+	var encrypted = encrypt.encrypt(texto);//||document.getElementById('input').value);
+	return encrypted;
+}
+function desencripta(texto){
+// Decrypt with the private key...
+          var decrypt = new JSEncrypt();
+          //decrypt.setPrivateKey(document.getElementById('privkey').value);
+          decrypt.setPrivateKey(clavePriv);
+          var uncrypted = decrypt.decrypt(texto);//||document.getElementById('output').value);
+
+          //document.getElementById('output2').value=uncrypted;
+	return uncrypted;
+}
+
+
+///parte nueva para generar con AES
+
+const generaDatos=()=>{
+	let claveAes='';
+	let aux='';
+	for (let i=0;i<10;i++){
+		aux=''+Math.floor(10000000000*Math.random());
+		aux='0000000000'.substring(0,10-aux.length)+aux;
+		claveAes+=aux;
+	}
+	claveAes=Aes.Ctr.encryptFijo(claveAes,claveAes,256);
+	let AES=claveAes;
+	let encr=EncrMensaje(claveAes);
+	let clave=calcMD5(claveAes);
+	lee();
+	let datos={texto_centro, tipo, nombre_centro,loc_centro, cod_centro,
+		Ap11, Ap12, Nombre1, Edad1,
+		Ap21, Ap22, Nombre2, Edad2,
+		Ap31, Ap32, Nombre3, Edad3,
+		Ap41, Ap42, Nombre4, Edad4,
+		ApP1, ApP2, NomP, TelP1, TelP2, EmailP}
+	let valor=Aes.Ctr.encryptFijo(JSON.stringify(datos),claveAes,256);
+	let dcc=Aes.Ctr.decrypt(valor,claveAes,256);
+	return {AES,encr,clave,datos: JSON.stringify(datos),valor,dcc};
+}
+
